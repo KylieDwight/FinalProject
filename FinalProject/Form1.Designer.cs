d4dafd58f02916539cf5c0288e5861e286db7381
DSU <DSU@PF3TPHGG>
2024-04-26 20:15:45 -0500

    Add project files.

---
+++ FinalProject/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Escape1 = new System.Windows.Forms.Button();
+            this.clock_label = new System.Windows.Forms.Label();
+            this.flower_label = new System.Windows.Forms.Label();
+            this.wrench_label = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Wrench_pb = new System.Windows.Forms.PictureBox();
+            this.Flower_pb = new System.Windows.Forms.PictureBox();
+            this.Clock_pb = new System.Windows.Forms.PictureBox();
+            this.background = new System.Windows.Forms.PictureBox();
+            this.clock_1_found = new System.Windows.Forms.Label();
+            this.wrench_1_found = new System.Windows.Forms.Label();
+            this.flower_2_found = new System.Windows.Forms.Label();
+            this.wrench_2_found = new System.Windows.Forms.Label();
+            this.clock_3_found = new System.Windows.Forms.Label();
+            this.flower_1_found = new System.Windows.Forms.Label();
+            this.clock_2_found = new System.Windows.Forms.Label();
+            this.clock_label_2 = new System.Windows.Forms.Label();
+            this.flower_label_2 = new System.Windows.Forms.Label();
+            this.wrench_label_2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Wrench_pb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower_pb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Clock_pb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Escape1
+            // 
+            this.Escape1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Escape1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Escape1.FlatAppearance.BorderSize = 0;
+            this.Escape1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Escape1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Escape1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Escape1.Location = new System.Drawing.Point(663, 362);
+            this.Escape1.Name = "Escape1";
+            this.Escape1.Size = new System.Drawing.Size(83, 34);
+            this.Escape1.TabIndex = 8;
+            this.Escape1.Tag = "Slide1";
+            this.Escape1.Text = "ESCAPE";
+            this.Escape1.UseVisualStyleBackColor = false;
+            this.Escape1.Visible = false;
+            this.Escape1.Click += new System.EventHandler(this.Escape1_Click);
+            // 
+            // clock_label
+            // 
+            this.clock_label.AutoSize = true;
+            this.clock_label.Location = new System.Drawing.Point(816, 144);
+            this.clock_label.Name = "clock_label";
+            this.clock_label.Size = new System.Drawing.Size(91, 20);
+            this.clock_label.TabIndex = 15;
+            this.clock_label.Tag = "Slide1";
+            this.clock_label.Text = "Parts        /3";
+            // 
+            // flower_label
+            // 
+            this.flower_label.AutoSize = true;
+            this.flower_label.Location = new System.Drawing.Point(816, 324);
+            this.flower_label.Name = "flower_label";
+            this.flower_label.Size = new System.Drawing.Size(91, 20);
+            this.flower_label.TabIndex = 16;
+            this.flower_label.Tag = "Slide1";
+            this.flower_label.Text = "Parts        /2";
+            // 
+            // wrench_label
+            // 
+            this.wrench_label.AutoSize = true;
+            this.wrench_label.Location = new System.Drawing.Point(816, 480);
+            this.wrench_label.Name = "wrench_label";
+            this.wrench_label.Size = new System.Drawing.Size(91, 20);
+            this.wrench_label.TabIndex = 17;
+            this.wrench_label.Tag = "Slide1";
+            this.wrench_label.Text = "Parts        /2";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // Wrench_pb
+            // 
+            this.Wrench_pb.BackgroundImage = global::FinalProject.Properties.Resources.Wrench;
+            this.Wrench_pb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Wrench_pb.Location = new System.Drawing.Point(767, 347);
+            this.Wrench_pb.Name = "Wrench_pb";
+            this.Wrench_pb.Size = new System.Drawing.Size(140, 153);
+            this.Wrench_pb.TabIndex = 14;
+            this.Wrench_pb.TabStop = false;
+            this.Wrench_pb.Tag = "Slide1";
+            this.Wrench_pb.Visible = false;
+            // 
+            // Flower_pb
+            // 
+            this.Flower_pb.BackgroundImage = global::FinalProject.Properties.Resources.Flower1;
+            this.Flower_pb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Flower_pb.Location = new System.Drawing.Point(788, 179);
+            this.Flower_pb.Name = "Flower_pb";
+            this.Flower_pb.Size = new System.Drawing.Size(119, 162);
+            this.Flower_pb.TabIndex = 13;
+            this.Flower_pb.TabStop = false;
+            this.Flower_pb.Tag = "Slide1";
+            this.Flower_pb.Visible = false;
+            // 
+            // Clock_pb
+            // 
+            this.Clock_pb.BackColor = System.Drawing.Color.Transparent;
+            this.Clock_pb.BackgroundImage = global::FinalProject.Properties.Resources.Clock;
+            this.Clock_pb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Clock_pb.Location = new System.Drawing.Point(776, 12);
+            this.Clock_pb.Name = "Clock_pb";
+            this.Clock_pb.Size = new System.Drawing.Size(131, 129);
+            this.Clock_pb.TabIndex = 12;
+            this.Clock_pb.TabStop = false;
+            this.Clock_pb.Tag = "Slide1";
+            this.Clock_pb.Visible = false;
+            // 
+            // background
+            // 
+            this.background.Image = global::FinalProject.Properties.Resources.Background_1;
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(1900, 500);
+            this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            this.background.Click += new System.EventHandler(this.background_Click);
+            // 
+            // clock_1_found
+            // 
+            this.clock_1_found.AutoSize = true;
+            this.clock_1_found.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.clock_1_found.Location = new System.Drawing.Point(-4, 266);
+            this.clock_1_found.Name = "clock_1_found";
+            this.clock_1_found.Size = new System.Drawing.Size(55, 20);
+            this.clock_1_found.TabIndex = 19;
+            this.clock_1_found.Tag = "Slide1";
+            this.clock_1_found.Text = "Found";
+            this.clock_1_found.Visible = false;
+            // 
+            // wrench_1_found
+            // 
+            this.wrench_1_found.AutoSize = true;
+            this.wrench_1_found.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.wrench_1_found.Location = new System.Drawing.Point(127, 179);
+            this.wrench_1_found.Name = "wrench_1_found";
+            this.wrench_1_found.Size = new System.Drawing.Size(55, 20);
+            this.wrench_1_found.TabIndex = 20;
+            this.wrench_1_found.Tag = "Slide1";
+            this.wrench_1_found.Text = "Found";
+            this.wrench_1_found.Visible = false;
+            // 
+            // flower_2_found
+            // 
+            this.flower_2_found.AutoSize = true;
+            this.flower_2_found.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.flower_2_found.Location = new System.Drawing.Point(309, 50);
+            this.flower_2_found.Name = "flower_2_found";
+            this.flower_2_found.Size = new System.Drawing.Size(55, 20);
+            this.flower_2_found.TabIndex = 21;
+            this.flower_2_found.Tag = "Slide1";
+            this.flower_2_found.Text = "Found";
+            this.flower_2_found.Visible = false;
+            // 
+            // wrench_2_found
+            // 
+            this.wrench_2_found.AutoSize = true;
+            this.wrench_2_found.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.wrench_2_found.Location = new System.Drawing.Point(548, 67);
+            this.wrench_2_found.Name = "wrench_2_found";
+            this.wrench_2_found.Size = new System.Drawing.Size(55, 20);
+            this.wrench_2_found.TabIndex = 22;
+            this.wrench_2_found.Tag = "Slide1";
+            this.wrench_2_found.Text = "Found";
+            this.wrench_2_found.Visible = false;
+            // 
+            // clock_3_found
+            // 
+            this.clock_3_found.AutoSize = true;
+            this.clock_3_found.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.clock_3_found.Location = new System.Drawing.Point(675, 90);
+            this.clock_3_found.Name = "clock_3_found";
+            this.clock_3_found.Size = new System.Drawing.Size(55, 20);
+            this.clock_3_found.TabIndex = 23;
+            this.clock_3_found.Tag = "Slide1";
+            this.clock_3_found.Text = "Found";
+            this.clock_3_found.Visible = false;
+            // 
+            // flower_1_found
+            // 
+            this.flower_1_found.AutoSize = true;
+            this.flower_1_found.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.flower_1_found.Location = new System.Drawing.Point(559, 198);
+            this.flower_1_found.Name = "flower_1_found";
+            this.flower_1_found.Size = new System.Drawing.Size(55, 20);
+            this.flower_1_found.TabIndex = 24;
+            this.flower_1_found.Tag = "Slide1";
+            this.flower_1_found.Text = "Found";
+            this.flower_1_found.Visible = false;
+            // 
+            // clock_2_found
+            // 
+            this.clock_2_found.AutoSize = true;
+            this.clock_2_found.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.clock_2_found.Location = new System.Drawing.Point(490, 347);
+            this.clock_2_found.Name = "clock_2_found";
+            this.clock_2_found.Size = new System.Drawing.Size(55, 20);
+            this.clock_2_found.TabIndex = 25;
+            this.clock_2_found.Tag = "Slide1";
+            this.clock_2_found.Text = "Found";
+            this.clock_2_found.Visible = false;
+            // 
+            // clock_label_2
+            // 
+            this.clock_label_2.AutoSize = true;
+            this.clock_label_2.Location = new System.Drawing.Point(851, 144);
+            this.clock_label_2.Name = "clock_label_2";
+            this.clock_label_2.Size = new System.Drawing.Size(18, 20);
+            this.clock_label_2.TabIndex = 26;
+            this.clock_label_2.Tag = "Slide1";
+            this.clock_label_2.Text = "0";
+            // 
+            // flower_label_2
+            // 
+            this.flower_label_2.AutoSize = true;
+            this.flower_label_2.Location = new System.Drawing.Point(851, 324);
+            this.flower_label_2.Name = "flower_label_2";
+            this.flower_label_2.Size = new System.Drawing.Size(18, 20);
+            this.flower_label_2.TabIndex = 27;
+            this.flower_label_2.Tag = "Slide1";
+            this.flower_label_2.Text = "0";
+            // 
+            // wrench_label_2
+            // 
+            this.wrench_label_2.AutoSize = true;
+            this.wrench_label_2.Location = new System.Drawing.Point(851, 480);
+            this.wrench_label_2.Name = "wrench_label_2";
+            this.wrench_label_2.Size = new System.Drawing.Size(18, 20);
+            this.wrench_label_2.TabIndex = 28;
+            this.wrench_label_2.Tag = "Slide1";
+            this.wrench_label_2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(928, 499);
+            this.Controls.Add(this.wrench_label_2);
+            this.Controls.Add(this.flower_label_2);
+            this.Controls.Add(this.clock_label_2);
+            this.Controls.Add(this.clock_2_found);
+            this.Controls.Add(this.flower_1_found);
+            this.Controls.Add(this.clock_3_found);
+            this.Controls.Add(this.wrench_2_found);
+            this.Controls.Add(this.flower_2_found);
+            this.Controls.Add(this.wrench_1_found);
+            this.Controls.Add(this.clock_1_found);
+            this.Controls.Add(this.wrench_label);
+            this.Controls.Add(this.flower_label);
+            this.Controls.Add(this.clock_label);
+            this.Controls.Add(this.Wrench_pb);
+            this.Controls.Add(this.Flower_pb);
+            this.Controls.Add(this.Clock_pb);
+            this.Controls.Add(this.Escape1);
+            this.Controls.Add(this.background);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(1900, 555);
+            this.MinimumSize = new System.Drawing.Size(950, 555);
+            this.Name = "Form1";
+            this.Text = "Escape Room";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Wrench_pb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Flower_pb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Clock_pb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.Button Escape1;
+        private System.Windows.Forms.PictureBox Clock_pb;
+        private System.Windows.Forms.PictureBox Flower_pb;
+        private System.Windows.Forms.PictureBox Wrench_pb;
+        private System.Windows.Forms.Label clock_label;
+        private System.Windows.Forms.Label flower_label;
+        private System.Windows.Forms.Label wrench_label;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label clock_1_found;
+        private System.Windows.Forms.Label wrench_1_found;
+        private System.Windows.Forms.Label flower_2_found;
+        private System.Windows.Forms.Label wrench_2_found;
+        private System.Windows.Forms.Label clock_3_found;
+        private System.Windows.Forms.Label flower_1_found;
+        private System.Windows.Forms.Label clock_2_found;
+        private System.Windows.Forms.Label clock_label_2;
+        private System.Windows.Forms.Label flower_label_2;
+        private System.Windows.Forms.Label wrench_label_2;
+    }
+}
+
---
+++ FinalProject/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+
+        int[] clock_pieces = { 0, 0, 0 };
+        int[] flower_pieces = { 0, 0 };
+        int[] wrench_pieces = { 0, 0 };
+        int clockp = 0;
+        int flowerp = 0;
+        int wrenchp = 0;
+
+
+        bool escape_1 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+          
+
+            if (clock_pieces[0] == 1 && clock_pieces[1] == 1 && clock_pieces[2] == 1)
+                Clock_pb.Visible = true;
+            if (wrench_pieces[0] == 1 && wrench_pieces[1] == 1)
+                Wrench_pb.Visible = true;
+            if (flower_pieces[0] == 1 && flower_pieces[1] == 1)
+                Flower_pb.Visible = true;
+
+            if (Clock_pb.Visible == true && Wrench_pb.Visible == true && Flower_pb.Visible == true)
+            {
+                Escape1.Visible = true;
+                Escape1.Enabled = true;
+            }
+
+            clock_label_2.Text = clockp.ToString();
+            flower_label_2.Text = flowerp.ToString();
+            wrench_label_2.Text = wrenchp.ToString();
+            
+
+        }
+
+        private void Escape1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Congradulations!");
+            this.Close();
+        }
+
+        private void background_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs mouseEvent = (MouseEventArgs)e;
+
+            //Clock 1
+            if (mouseEvent.X >= 0 && mouseEvent.X <= 0 + 39
+                && mouseEvent.Y >= 249 && mouseEvent.Y <= 249 + 34
+                && escape_1 == false)
+            {
+                clockp += 1;
+
+                clock_pieces[0] = 1;
+                clock_1_found.Visible = true;
+            }
+
+            //Clock 2
+            if (mouseEvent.X >= 494 && mouseEvent.X <= 494 + 39
+                && mouseEvent.Y >= 345 && mouseEvent.Y <= 345 + 34
+                && escape_1 == false)
+            {
+                clockp += 1;
+
+                clock_pieces[1] = 1;
+                clock_2_found.Visible = true;
+            }
+
+            //Clock 3
+            if (mouseEvent.X >= 678 && mouseEvent.X <= 678 + 39
+                && mouseEvent.Y >= 88 && mouseEvent.Y <= 88 + 34
+                && escape_1 == false)
+            {
+                clockp += 1;
+
+                clock_pieces[2] = 1;
+                clock_3_found.Visible = true;
+            }
+
+            //Flower 1
+            if (mouseEvent.X >= 567 && mouseEvent.X <= 567 + 39
+                && mouseEvent.Y >= 194 && mouseEvent.Y <= 194 + 34
+                && escape_1 == false)
+            {
+                flowerp += 1;
+
+                flower_pieces[0] = 1;
+                flower_1_found.Visible = true;
+            }
+
+            //Flower 2
+            if (mouseEvent.X >= 312 && mouseEvent.X <= 312 + 36
+                && mouseEvent.Y >= 38 && mouseEvent.Y <= 38 + 34
+                && escape_1 == false)
+            {
+                flowerp += 1;
+
+                flower_pieces[1] = 1;
+                flower_2_found.Visible = true;
+            }
+
+            //Wrench 1
+            if (mouseEvent.X >= 138 && mouseEvent.X <= 138 + 31
+                && mouseEvent.Y >= 179 && mouseEvent.Y <= 179 + 25
+                && escape_1 == false)
+            {
+                wrenchp += 1;
+
+                wrench_pieces[0] = 1;
+                wrench_1_found.Visible = true;
+            }
+
+            //Wrench 2
+            if (mouseEvent.X >= 567 && mouseEvent.X <= 567 + 24
+                && mouseEvent.Y >= 50 && mouseEvent.Y <= 50 + 60
+                && escape_1 == false)
+            {
+                wrenchp += 1;
+
+                wrench_pieces[1] = 1;
+                wrench_2_found.Visible = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
